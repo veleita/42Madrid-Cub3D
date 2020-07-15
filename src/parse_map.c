5c142c51b76fd53bd59d2088fe51294191991240
mzomeno- <1veleita1@gmail.com>
2020-07-15 17:47:22 +0200

    Added bonus rules (not tested) 	modified:   Makefile
    Music (not tested)
    	new file:   bonus/music.c
    
    New map
    	new file:   maps/no_sprite.cub
    
    Collisions
    	modified:   src/movement.c
    	modified:   src/parse_map.c

--- src/parse_map.c
+++ src/parse_map.c
@@ -84,7 +84,8 @@ t_camera		*check_coord(char coord, int pos_x, int pos_y)
 
 void			valid_map(t_map *map, int y, int x)
 {
-	if ((map->map[y][x] == 0 && (x == (map->x - 1) || y == (map->y - 1) ||
+	if (((map->map[y][x] == 0 || map->map[y][x] == 2) &&
+			       		(x == (map->x - 1) || y == (map->y - 1) ||
 					x == 0 || y == 0)) || map->map[y][x] == ' ')
 		ft_exit_fail("Invalid map");
 	map->map[y][x] = map->map[y][x] == 0 ? 3 : 1;
